e9e232f31f7a7d758eb24680c6f0e7a13055b0ac
Your Name <your.email@example.com>
2024-01-28 12:21:30 +0530

    Fix: improved some major animations

--- main.js
+++ main.js
@@ -1,80 +1,53 @@
-TweenMax.staggerFrom(
-  ".navbar > div",
-  0.8,
-  {
-    x: "-80",
-    opacity: 0,
-    ease: Power3.easeOut,
-    delay: 1,
-  },
-  0.08
-);
+//main page nav tabs animation
 
-TweenMax.staggerFrom("ul > li", 1, {
+TweenMax.staggerFrom("ul", 1, {
   y: "-50",
   opacity: 0,
   ease: Power3.easeOut,
-  delay: 2.5,
-  stagger: 0.25,
+  delay: 3.5,
 });
 
-TweenMax.staggerFrom(
-  ".logo > svg",
-  1,
-  {
-    y: "-50",
-    opacity: 0,
-    ease: Power3.easeOut,
-    delay: 10,
-  },
-  0.08
-);
+//slider animations
 
 document.addEventListener("DOMContentLoaded", function () {
-  TweenMax.to(".overlay", 1.4, {
+  TweenMax.to(".overlay", 0, {
     top: 0, // Slide the overlay in
     ease: Expo.easeOut,
     onComplete: function () {
       setTimeout(function () {
         // Pause for 1 second before sliding out
-        TweenMax.to(".overlay", 2.5, {
+        TweenMax.to(".overlay", 2.8, {
           top: "-100%",
           ease: Expo.easeOut,
         });
-      }, 1000); // Adjust the delay time as needed
+      }, 2000);
     },
   });
 });
 
-var textWrapper1 = document.querySelector(".name");
-var textWrapper2 = document.querySelector(".greet");
-textWrapper1.innerHTML = textWrapper1.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
-textWrapper2.innerHTML = textWrapper2.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
-anime.timeline().add({
-  targets: ".name .letter",
-  translateY: [100, 0],
-  translateZ: 0,
-  easing: "easeOutExpo",
-  duration: 2000,
-  stagger: 5,
-  delay: (el, i) => 2600 + 40 * i,
-});
-anime.timeline().add({
-  targets: ".greet .letter",
-  translateY: [100, 0],
-  translateZ: 0,
-  easing: "easeOutExpo",
-  duration: 2000,
-  stagger: 5,
-  delay: (el, i) => 2600 + 40 * i,
-});
+// overlay texts animations
 
+var tl = gsap.timeline({ defaults: { ease: "SlowMo.easeOut" } });
+
+tl.to(".overlay-content", { y: "0%", duration: 0.8, stagger: 0.15 });
+
+//logo animations
+tl.to(".overlay-content", { y: "0%", duration: 1.2, stagger: 0.5 });
+tl.fromTo(
+  ".logo svg",
+  { y: "-150%" },
+  { y: "0%", duration: 1.2, delay: 0.15, ease: "power3.easeOut" }
+);
+
+//hero about animations
+tl.to(".p-hide", { y: "0%", duration: 0.3, stagger: 0.15 });
+
+/////////////////////////////////// TOOO //////////////////////////////////////
+// 1. SCROLL-ANIMATION FOR ABOUT TITLE
+//1. tl.to(".about-title-hide", { y: "0%", duration: 0.6, stagger: 0.15 });
+//////////////////////////////////////////////////////////////////////////////
+
+//mouse pointer animations
 var cursor = document.querySelector(".cursor");
 var cursorBlur = document.querySelector(".cursor-blur");
 
@@ -83,4 +56,22 @@ document.addEventListener("mousemove", function (dets) {
   cursor.style.top = dets.y + "px";
   cursorBlur.style.left = dets.x - 250 + "px";
   cursorBlur.style.top = dets.y - 250 + "px";
+});
+
+// hero animations
+const myText = new SplitType(".greet");
+const myText2 = new SplitType(".name");
+
+gsap.to(".greet .char", {
+  y: 0,
+  stagger: 0.05,
+  delay: 2.3,
+  duration: 0.2,
+});
+
+gsap.to(".name .char", {
+  y: 0,
+  stagger: 0.05,
+  delay: 2.3,
+  duration: 0.2,
 });
